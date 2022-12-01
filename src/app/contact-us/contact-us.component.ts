36ab0e5b8977d8d6120a845ccd0f9ae12cc1918e
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-01 09:31:23 +0530

    bugfix

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  name = ""
+  designation = ""
+  phoneNumber = ""
+  email = ""
+  subject = ""
 
+  readValues = () => {
+
+    let data: any = {
+      "name": this.name,
+      "designation": this.designation,
+      "phoneNumber": this.phoneNumber,
+      "email": this.email,
+      "subject": this.subject
+    }
+    console.log(data)
+  }
 }
